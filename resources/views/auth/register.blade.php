912ddfa1c2d8e956af129ea1cbe6b9fd92cb170f
Bayu Sumantri <bayusuman09@gmail.com>
2023-10-05 12:06:57 +0700

    On Progres

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Login V2</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="{{ asset('/Login_V2/images/icons/favicon.ico') }}" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('/Login_V2/vendor/bootstrap/css/bootstrap.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css"
+        href="{{ asset('/Login_V2/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css"
+        href="{{ asset('/Login_V2/fonts/iconic/css/material-design-iconic-font.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('/Login_V2/vendor/animate/animate.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('/Login_V2/vendor/css-hamburgers/hamburgers.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('/Login_V2/vendor/animsition/css/animsition.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('/Login_V2/vendor/select2/select2.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('/Login_V2/vendor/daterangepicker/daterangepicker.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('/Login_V2/css/util.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/Login_V2/css/main.css') }}">
+    <!--===============================================================================================-->
+</head>
+
+<body>
+
+    <div class="limiter">
+        <div class="container-login100">
+            <div class="wrap-login100">
+                <form class="login100-form validate-form" action="{{ route('register') }}" method="POST">
+                    @csrf
+                    <span class="login100-form-title p-b-26">
+                        Login
+                    </span>
+                    <span class="login100-form-title p-b-48">
+                        <i class="zmdi zmdi-memory"></i>
+                    </span>
+
+                    <div class="wrap-input100 validate-input" data-validate="Valid nama is: abshv">
+                        <input-label for="nama" :value="__('Name')" />
+                        <input class="input100" type="text" name="name" requiredautofocus autocomplete="username"
+                            :value="old('nama')">
+                        <span class="focus-input100" data-placeholder="Nama"></span>
+                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                    </div>
+
+                    <div class="wrap-input100 validate-input" data-validate="Valid email is: a@b.c">
+                        <input-label for="email" :value="__('Email')" />
+                        <input class="input100" type="email" name="email" requiredautofocus autocomplete="username"
+                            :value="old('email')">
+                        <span class="focus-input100" data-placeholder="Email"></span>
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+
+                    <div class="wrap-input100 validate-input" data-validate="Enter password">
+                        <span class="btn-show-pass">
+                            <i class="zmdi zmdi-eye"></i>
+                        </span>
+                        <input-label for="password" :value="__('Password')" />
+                        <input class="input100" type="password" name="password" id="inputPassword" required>
+                        <span class="focus-input100" data-placeholder="Password"></span>
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div>
+
+                    <div class="wrap-input100 validate-input" data-validate="Enter password">
+                        <span class="btn-show-pass">
+                            <i class="zmdi zmdi-eye"></i>
+                        </span>
+                        <input-label for="password_confirmation" :value="__('Confirm Password')" />
+                        <input class="input100" type="password" name="password_confirmation" required
+                            autocomplete="new-password" id="inputPassword" required>
+                        <span class="focus-input100" data-placeholder="Password"></span>
+                        <input-error :messages="$errors - > get('password_confirmation')" class="mt-2" />
+                    </div>
+
+                    <div class="container-login100-form-btn">
+                        <div class="wrap-login100-form-btn">
+                            <div class="login100-form-bgbtn"></div>
+                            <button class="login100-form-btn">
+                                Login
+                            </button>
+                        </div>
+                    </div>
+
+                    <div class="text-center p-t-115">
+                        <span class="txt1">
+                            Already have an account?
+                        </span>
+
+                        <a class="txt2" href="{{ route('login') }}">
+                            Log In!
+                        </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+    <div id="dropDownSelect1"></div>
+
+    <!--===============================================================================================-->
+    <script src="{{ asset('/Login_V2/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
+    <!--===============================================================================================-->
+    <script src="{{ asset('/Login_V2/vendor/animsition/js/animsition.min.js') }}"></script>
+    <!--===============================================================================================-->
+    <script src="{{ asset('/Login_V2/vendor/bootstrap/js/popper.js') }}"></script>
+    <script src="{{ asset('/Login_V2/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+    <!--===============================================================================================-->
+    <script src="{{ asset('/Login_V2/vendor/select2/select2.min.js') }}"></script>
+    <!--===============================================================================================-->
+    <script src="{{ asset('/Login_V2/vendor/daterangepicker/moment.min.js') }}"></script>
+    <script src="{{ asset('/Login_V2/vendor/daterangepicker/daterangepicker.js') }}"></script>
+    <!--===============================================================================================-->
+    <script src="{{ asset('/Login_V2/vendor/countdowntime/countdowntime.js') }}"></script>
+    <!--===============================================================================================-->
+    <script src="{{ asset('/Login_V2/js/main.js') }}"></script>
+
+</body>
+
+</html>
---
+++ resources/views/landing_pages/master.blade.php
@@ -0,0 +1,316 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Basic -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Site Metas -->
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <link rel="icon" href="{{ asset('/home/images/favicon.png') }}" type="icon">
+
+    <title> DadeFood </title>
+
+    <!-- bootstrap core css -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('/home/css/bootstrap.css') }}" />
+
+    <!--owl slider stylesheet -->
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+    <!-- nice select  -->
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/css/nice-select.min.css"
+        integrity="sha512-CruCP+TD3yXzlvvijET8wV5WxxEh5H8P4cmz0RFbKK6FlZ2sYl3AEsKlLPHbniXKSrDdFewhbmBK5skbdsASbQ=="
+        crossorigin="anonymous" />
+    <!-- font awesome style -->
+    <link href="{{ asset('/home/css/font-awesome.min.css') }}" rel="stylesheet" />
+
+    <!-- Custom styles for this template -->
+    <link href="{{ asset('/home/css/style.css') }}" rel="stylesheet" />
+    <!-- responsive style -->
+    <link href="{{ asset('/home/css/responsive.css') }}" rel="stylesheet" />
+
+</head>
+
+<body>
+
+    <div class="hero_area">
+        <div class="bg-box">
+            <img src="{{ asset('/home/images/hero-bg.jpg') }}" alt="">
+        </div>
+        @include('landing_pages.header')
+        <!-- slider section -->
+        <section class="slider_section ">
+            <div id="customCarousel1" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="container ">
+                            <div class="row">
+                                <div class="col-md-7 col-lg-6 ">
+                                    <div class="detail-box">
+                                        <h1>
+                                            Fast Food Restaurant
+                                        </h1>
+                                        <p>
+                                            Doloremque, itaque aperiam facilis rerum, commodi, temporibus sapiente ad
+                                            mollitia laborum quam quisquam esse error unde. Tempora ex doloremque,
+                                            labore, sunt repellat dolore, iste magni quos nihil ducimus libero ipsam.
+                                        </p>
+                                        <div class="btn-box">
+                                            <a href="{{ url('') }}" class="btn1">
+                                                Order Now
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item ">
+                        <div class="container ">
+                            <div class="row">
+                                <div class="col-md-7 col-lg-6 ">
+                                    <div class="detail-box">
+                                        <h1>
+                                            Fast Food Restaurant
+                                        </h1>
+                                        <p>
+                                            Doloremque, itaque aperiam facilis rerum, commodi, temporibus sapiente ad
+                                            mollitia laborum quam quisquam esse error unde. Tempora ex doloremque,
+                                            labore, sunt repellat dolore, iste magni quos nihil ducimus libero ipsam.
+                                        </p>
+                                        <div class="btn-box">
+                                            <a href="{{ url('') }}" class="btn1">
+                                                Order Now
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="container ">
+                            <div class="row">
+                                <div class="col-md-7 col-lg-6 ">
+                                    <div class="detail-box">
+                                        <h1>
+                                            Fast Food Restaurant
+                                        </h1>
+                                        <p>
+                                            Doloremque, itaque aperiam facilis rerum, commodi, temporibus sapiente ad
+                                            mollitia laborum quam quisquam esse error unde. Tempora ex doloremque,
+                                            labore, sunt repellat dolore, iste magni quos nihil ducimus libero ipsam.
+                                        </p>
+                                        <div class="btn-box">
+                                            <a href="{{ url('') }}" class="btn1">
+                                                Order Now
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="container">
+                    <ol class="carousel-indicators">
+                        <li data-target="#customCarousel1" data-slide-to="0" class="active"></li>
+                        <li data-target="#customCarousel1" data-slide-to="1"></li>
+                        <li data-target="#customCarousel1" data-slide-to="2"></li>
+                    </ol>
+                </div>
+            </div>
+
+        </section>
+        <!-- end slider section -->
+    </div>
+
+    @yield('landing_pages')
+
+
+    <!-- about section -->
+
+    <section class="about_section layout_padding">
+        <div class="container  ">
+
+            <div class="row">
+                <div class="col-md-6 ">
+                    <div class="img-box">
+                        <img src="{{ asset('/home/images/about-img.png') }}" alt="">
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="detail-box">
+                        <div class="heading_container">
+                            <h2>
+                                We Are Feane
+                            </h2>
+                        </div>
+                        <p>
+                            There are many variations of passages of Lorem Ipsum available, but the majority have
+                            suffered alteration
+                            in some form, by injected humour, or randomised words which don't look even slightly
+                            believable. If you
+                            are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything
+                            embarrassing hidden in
+                            the middle of text. All
+                        </p>
+                        <a href="{{ url('') }}">
+                            Read More
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- end about section -->
+
+    <!-- book section -->
+    <section class="book_section layout_padding">
+        <div class="container">
+            <div class="heading_container">
+                <h2>
+                    Book A Table
+                </h2>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form_container">
+                        <form action="">
+                            <div>
+                                <input type="text" class="form-control" placeholder="Your Name" />
+                            </div>
+                            <div>
+                                <input type="text" class="form-control" placeholder="Phone Number" />
+                            </div>
+                            <div>
+                                <input type="email" class="form-control" placeholder="Your Email" />
+                            </div>
+                            <div>
+                                <select class="form-control nice-select wide">
+                                    <option value="" disabled selected>
+                                        How many persons?
+                                    </option>
+                                    <option value="">
+                                        2
+                                    </option>
+                                    <option value="">
+                                        3
+                                    </option>
+                                    <option value="">
+                                        4
+                                    </option>
+                                    <option value="">
+                                        5
+                                    </option>
+                                </select>
+                            </div>
+                            <div>
+                                <input type="date" class="form-control">
+                            </div>
+                            <div class="btn_box">
+                                <button>
+                                    Book Now
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="map_container ">
+                        <div id="googleMap"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end book section -->
+
+    <!-- client section -->
+
+    <section class="client_section layout_padding-bottom">
+        <div class="container">
+            <div class="heading_container heading_center psudo_white_primary mb_45">
+                <h2>
+                    What Says Our Customers
+                </h2>
+            </div>
+            <div class="carousel-wrap row ">
+                <div class="owl-carousel client_owl-carousel">
+                    <div class="item">
+                        <div class="box">
+                            <div class="detail-box">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+                                </p>
+                                <h6>
+                                    Moana Michell
+                                </h6>
+                                <p>
+                                    magna aliqua
+                                </p>
+                            </div>
+                            <div class="img-box">
+                                <img src="{{ asset('/home/images/client1.jpg') }}" alt="" class="box-img">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="box">
+                            <div class="detail-box">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+                                </p>
+                                <h6>
+                                    Mike Hamell
+                                </h6>
+                                <p>
+                                    magna aliqua
+                                </p>
+                            </div>
+                            <div class="img-box">
+                                <img src="{{ asset('/home/images/client2.jpg') }}" alt="" class="box-img">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- end client section -->
+
+    @include('landing_pages.footer')
+
+    <!-- jQery -->
+    <script src="{{ asset('/home/js/jquery-3.4.1.min.js') }}"></script>
+    <!-- popper js -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <!-- bootstrap js -->
+    <script src="{{ asset('/home/js/bootstrap.js') }}"></script>
+    <!-- owl slider -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <!-- isotope js -->
+    <script src="https://unpkg.com/isotope-layout@3.0.4/dist/isotope.pkgd.min.js"></script>
+    <!-- nice select -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/js/jquery.nice-select.min.js"></script>
+    <!-- custom js -->
+    <script src="{{ asset('/home/js/custom.js') }}"></script>
+    <!-- Google Map -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh39n5U-4IoWpsVGUHWdqB6puEkhRLdmI&callback=myMap">
+    </script>
+    <!-- End Google Map -->
+
+</body>
+
+</html>
